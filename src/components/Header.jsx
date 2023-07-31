cb35c02051e2c005eb481959699c6adb4dea6533
ViktoriaV <august.flamenco@gmail.com>
2023-08-01 00:19:56 +0300

    correction in stack and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,13 +39,13 @@ export const Header = ({
           <span className="nav__item" onClick={(ev) => handleChangeFocus(ev, focusContact)}>Контакты</span>
         </nav>
 
-        <div class="nav__dropdown">
-        <svg class="nav__dropdown-icon" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
-          <div class="nav__dropdown-content">
-            <p class="nav__dropdown-item" onClick={(ev) => handleChangeFocus(ev, focusAbout)}>Обо мне</p>
-            <p class="nav__dropdown-item" onClick={(ev) => handleChangeFocus(ev, focusStack)}>Инструменты</p>
-            <p class="nav__dropdown-item" onClick={(ev) => handleChangeFocus(ev, focusPortfolio)}>Проекты</p>
-            <p class="nav__dropdown-item" onClick={(ev) => handleChangeFocus(ev, focusContact)}>Контакты</p>
+        <div className="nav__dropdown">
+        <svg className="nav__dropdown-icon" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+          <div className="nav__dropdown-content">
+            <p className="nav__dropdown-item" onClick={(ev) => handleChangeFocus(ev, focusAbout)}>Обо мне</p>
+            <p className="nav__dropdown-item" onClick={(ev) => handleChangeFocus(ev, focusStack)}>Инструменты</p>
+            <p className="nav__dropdown-item" onClick={(ev) => handleChangeFocus(ev, focusPortfolio)}>Проекты</p>
+            <p className="nav__dropdown-item" onClick={(ev) => handleChangeFocus(ev, focusContact)}>Контакты</p>
           </div>
         </div>
       </div>
